a9204322ce5685dbe5d57b3c483660c9e762867b
kirandash <thekirankumardash@gmail.com>
2016-09-11 13:12:24 +0530

    004 Ajax Post Script

--- includes/newsletter-subscriber-scripts.php
+++ includes/newsletter-subscriber-scripts.php
@@ -3,7 +3,7 @@
 // Add scripts
 function ns_add_script() {
 	wp_enqueue_style( 'ns-main-style', plugins_url() . '/newsletter-subscriber/css/style.css' );
-	wp_enqueue_script( 'ns-main-script', plugins_url() . '/newsletter-subscriber/js/main.js' );
+	wp_enqueue_script( 'ns-main-script', plugins_url() . '/newsletter-subscriber/js/main.js', array('jquery'), '1.0.0', true );
 }
 
 add_action( 'wp_enqueue_scripts', 'ns_add_script' );
--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
-// JavaScript Document
+jQuery(document).ready(function($) {
+
+	$('#subscriber-form').submit(function(e){
+		e.preventDefault();
+		
+		// Serialize form
+		var subscriberData = $('#subscriber-form').serialize();
+		
+		// Submit Form
+		$.ajax({
+			type: 'post',
+			url: $('#subscriber-form').attr('action'),
+			data: subscriberData
+		}).done(function(response){
+			// If success
+			$('#form-msg').removeClass('error');
+			$('#form-msg').addClass('success');
+			
+			// Set Message text
+			$('#form-msg').text(response);
+			
+			// Clear Fields
+			$('#name').val('');
+			$('#email').val('');
+		}).fail(function(){
+			// If Error
+			$('#form-msg').removeClass('success');
+			$('#form-msg').addClass('error');
+			
+			if(data.responseText != ''){
+				// Set Message text
+				$('#form-msg').text(response);	
+			} else {
+				$('#form-msg').text('Message was not sent');
+			}
+						
+		});
+		
+	});
+});
